cd234f194d97b32b6c584a7d32c72fc4ab5778fd
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-03-30 19:40:20 -0500

    Delete Method Added

--- main.js
+++ main.js
@@ -69,6 +69,17 @@ function validateCourse(course) {
   return Joi.ValidationError(course, schema);
 }
 
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send('The course with the given Id was not found');
+
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 });
